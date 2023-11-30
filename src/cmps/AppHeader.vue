0ef3369851f639af5c15d4db6cb826aeaf37e5c0
itamarsa <itamarr87@gmail.com>
2023-11-30 19:16:12 +0200

    F/create-CRUDL/add appheader

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -1,5 +1,6 @@
 <template>
-    <header>
+    <header class="app-header">
+        <a href="/"><h2>Mr Bitcoin</h2></a>
         <nav>
             <RouterLink to="/">Home</RouterLink> |
             <RouterLink to="/contact">Contacts</RouterLink> |
@@ -13,4 +14,22 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.app-header {
+    display: flex;
+    align-items: center;
+    justify-content:space-between;
+    height: 5vh;
+
+    padding: 10px;
+    background-color: rgb(101, 222, 101);
+
+    a {
+        text-decoration: none;
+    }
+
+    .router-link-exact-active {
+        color: white;
+    }
+}
+</style>
