33f6b392044e1dca62cb677d4c9b7f7577ba4f6b
itamarsa <itamarr87@gmail.com>
2023-11-30 20:36:32 +0200

    F/create-CRUDL/align code

--- src/pages/ContactEdit.vue
+++ src/pages/ContactEdit.vue
@@ -1,81 +1,20 @@
-<!-- <template>
-    <h1>Contact Edit</h1>
-    <section v-if="contact" class="contact-edit">
-        <form @submit.prevent="onSaveContact">
-            <input v-modal="contact.name" type="text" autofocus>
-            <input v-modal="contact.email" type="text">
-            <input v-modal="contact.phone" type="text">
-            <button>save</button>
-        </form>
-    </section>
-    <img v-else src="../assets/puff.svg" alt="" class="loader">
-</template>
-
-<script>
-import { contactService } from '../services/contact.service';
-import { eventBus } from '../services/eventBus.service'
-export default {
-    data(){
-        return {
-            contact: null
-        }
-
-    },
-    methods: {
-        async onSaveContact() {
-            const isUpdate = !!this.$route.params.contactId
-            await contactService.save(this.contact)
-
-            if(isUpdate){
-                eventBus.emit('user-msg', 'Contact Updated')
-            } else {
-                eventBus.emit('user-msg', 'Contact Saved')
-            }
-            this.$router.push('/contact')
-        }
-    },
-    async created () {
-        const contactId = this.$route.params.contactId
-        
-        if(contactId) {
-            this.contact = await contactService.get(contactId)
-        } else {
-            this.contact = contactService.getEmptyContact()
-        }
-
-    }
-}
-</script>
-
-<style lang="scss">
-.contact-edit {
-    display: grid;
-    padding: 10px;
-    background-color: darkseagreen;
-
-    a {
-        justify-self: end;
-    }
-}
-
-</style> -->
 <template>
-    <h1>
-        contact edit
-    </h1>
+    <h1>Contact edit</h1>
     <section v-if="contact" class="contact-edit">
         <form @submit.prevent="onSaveContact">
             <input placeholder="Name" v-model="contact.name" type="text" autofocus>
-            <input  placeholder="email" v-model="contact.email" type="text">
-            <input  placeholder="Phone" v-model="contact.phone" type="text">
+            <input placeholder="email" v-model="contact.email" type="text">
+            <input placeholder="Phone" v-model="contact.phone" type="text">
             <button>Save</button>
         </form>
     </section>
     <img v-else src="../assets/puff.svg" alt="" class="loader">
 </template>
+
 <script>
 import { contactService } from '../services/contact.service'
 import { eventBus } from '../services/eventBus.service'
+
 export default {
     data() {
         return {
@@ -86,7 +25,8 @@ export default {
         async onSaveContact() {
             const isUpdate = !!this.$route.params.contactId
             await contactService.save(this.contact)
-            if(isUpdate){
+
+            if (isUpdate) {
                 eventBus.emit('user-msg', 'Contact Updated')
             } else {
                 eventBus.emit('user-msg', 'Contact Saved')
@@ -94,21 +34,23 @@ export default {
             this.$router.push('/contact')
         }
     },
-    async created(){
-        const contactId =this.$route.params.contactId
+    async created() {
+        const contactId = this.$route.params.contactId
         if (contactId) {
             this.contact = await contactService.get(contactId)
-        }else{
+        } else {
             this.contact = contactService.getEmptyContact()
         }
     }
 }
 </script>
+
 <style lang="scss">
 .contact-edit {
     display: grid;
     padding: 10px;
     background-color: darkseagreen;
+
     a {
         justify-self: end;
     }
