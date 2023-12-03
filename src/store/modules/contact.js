89181aaf1f1a80c0f462a48f2cbea066a755b0f4
itamarsa <itamarr87@gmail.com>
2023-12-03 15:03:25 +0200

    F/Transform-to-store/remove+save+edit+add

--- src/store/modules/contact.js
+++ src/store/modules/contact.js
@@ -8,26 +8,59 @@ export default {
         }
     },
     mutations: {
-        setContacts(state, {contacts}){
+        setContacts(state, { contacts }) {
             state.contacts = contacts
-        }
+        },
+        removeContact(state, { contactId }) {
+            const idx = state.contacts.findIndex(contact => contact._id === contactId)
+            state.contacts.splice(idx, 1)
+    
+        },
+        updateContact(state, {savedContact}){
+            const idx = state.contacts.findIndex(contact => contact._id === savedContact._id)
+            state.contacts.splice(idx, 1, savedContact)
+        },
+        saveContact(state, {savedContact}){
+            state.contacts.push(savedContact)
+        } 
     },
     actions: {
-        async loadContacts(context){
+        async loadContacts(context) {
             try {
                 const contacts = await contactService.query()
-                context.commit({ type: 'setContacts', contacts})
-            } catch(err){
+                context.commit({ type: 'setContacts', contacts })
+            } catch (err) {
+                console.log(err)
+                throw err
+            }
+        },
+        async removeContact(context, {contactId}){
+            try {
+                await contactService.remove(contactId)
+                context.commit({type: 'removeContact', contactId})
+            } catch(err) {
+                console.log(err)
+                throw err
+            }
+        },
+        async saveContact(context, {contact}){
+            const isUpdate = !!contact._id
+
+            try {
+                const savedContact = await contactService.save(contact)
+
+                if (isUpdate) {
+                    context.commit({type: 'updateContact', savedContact})
+                } else {
+                    context.commit({type: 'saveContact', savedContact})
+                } 
+            } catch (err){
                 console.log(err)
                 throw err
             }
         }
-
     },
     getters: {
-
-    },
-    modules: {
-
-    },
+        contacts(state) { return state.contacts }
+    }
 }
