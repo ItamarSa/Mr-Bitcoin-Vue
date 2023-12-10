0b1b5d3600a2cf018fc70cd9a582d4d77a8bf4d8
itamarsa <itamarr87@gmail.com>
2023-12-10 10:47:22 +0200

    F/clean-code/remove pei chart

--- src/pages/Charts.vue
+++ src/pages/Charts.vue
@@ -1,21 +1,15 @@
 <template>
     <div class="container">
-        <!-- Display Bar charts -->
         <Bar v-if="loaded.marketPrice" class="bar" :data="chartData.marketPrice" :options="chartOptions" />
-        <!-- <Bar v-if="loaded.avgBlockSize" class="bar" :data="chartData.avgBlockSize" :options="chartOptions" /> -->
-        <!-- Display Line chart for avgBlockSize -->
         <Line v-if="loaded.avgBlockSize" class="line" :data="chartData.avgBlockSize" :options="chartOptions" />
 
-
-        <!-- Display Pie chart -->
-        <Pie v-if="loaded.pieChart" :data="chartData.pieChart" :options="chartOptions" />
+        <!-- <Pie v-if="loaded.pieChart" :data="chartData.pieChart" :options="chartOptions" /> -->
         <img v-else src="../assets/puff.svg" alt="" class="loader">
     </div>
 </template>
   
 <script>
 import { Bar, Pie, Line } from 'vue-chartjs'
-
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement,PointElement, CategoryScale, LinearScale, ArcElement, LineElement } from 'chart.js'
 import { bitcoinService } from '../services/bitcoin.service'
 
@@ -43,20 +37,20 @@ export default {
     },
     async mounted() {
         try {
-            const marketPriceData = await bitcoinService.getMarketPriceHistory();
-            console.log('Market Price Data:', marketPriceData);
-            this.chartData.marketPrice = this.processChartData(marketPriceData);
-            this.loaded.marketPrice = true;
+            const marketPriceData = await bitcoinService.getMarketPriceHistory()
+            console.log('Market Price Data:', marketPriceData)
+            this.chartData.marketPrice = this.processChartData(marketPriceData)
+            this.loaded.marketPrice = true
 
-            const avgBlockSizeData = await bitcoinService.getAvgBlockSize();
-            console.log('Avg Block Size Data:', avgBlockSizeData);
-            this.chartData.avgBlockSize = this.processLineChartData(avgBlockSizeData); // Process as line chart
-            this.loaded.avgBlockSize = true;
+            const avgBlockSizeData = await bitcoinService.getAvgBlockSize()
+            console.log('Avg Block Size Data:', avgBlockSizeData)
+            this.chartData.avgBlockSize = this.processLineChartData(avgBlockSizeData)
+            this.loaded.avgBlockSize = true
 
-            const pieChartData = await bitcoinService.getMarketPriceHistory();
-            console.log('pie Chart Data:', pieChartData);
-            this.chartData.pieChart = this.processPieChartData(pieChartData);
-            this.loaded.pieChart = true;
+            const pieChartData = await bitcoinService.getMarketPriceHistory()
+            console.log('pie Chart Data:', pieChartData)
+            this.chartData.pieChart = this.processPieChartData(pieChartData)
+            this.loaded.pieChart = true
         } catch (error) {
             console.error(error)
         }
@@ -72,14 +66,11 @@ export default {
                     backgroundColor: 'rgba(255, 99, 132, 0.5)',
                     borderColor: 'rgba(255, 99, 132, 1)'
                 }
-                // Add more data names and their corresponding colors here
-            };
-
+            }
             const colorConfig = colorMap[data.name] || {
                 backgroundColor: 'rgba(0, 0, 0, 0.2)',
                 borderColor: 'rgba(0, 0, 0, 1)'
-            };
-
+            }
             return {
                 labels: data.values.map(entry => new Date(entry.x * 1000).toLocaleDateString()),
                 datasets: [
@@ -91,16 +82,8 @@ export default {
                         data: data.values.map(entry => entry.y)
                     }
                 ]
-            };
+            }
         },
-        //   async fetchPieChartData() {
-        //     try {
-        //       const response = await fetch('https://api.blockchain.info/charts/market-cap?timespan=5months&format=json&cors=true');
-        //       return await response.json();
-        //     } catch (error) {
-        //       console.error(error);
-        //     }
-        //   },
         processPieChartData(data) {
             return {
                 labels: data.values.map(entry => new Date(entry.x * 1000).toLocaleDateString()),
@@ -113,7 +96,6 @@ export default {
                             'rgba(255, 206, 86, 0.2)',
                             'rgba(75, 192, 192, 0.2)',
                             'rgba(153, 102, 255, 0.2)'
-                            // Add more colors if needed for different segments
                         ],
                         borderColor: [
                             'rgba(255, 99, 132, 1)',
@@ -121,20 +103,18 @@ export default {
                             'rgba(255, 206, 86, 1)',
                             'rgba(75, 192, 192, 1)',
                             'rgba(153, 102, 255, 1)'
-                            // Add more colors if needed for different segments
                         ],
                         borderWidth: 5,
                         data: data.values.map(entry => entry.y)
                     }
                 ]
-            };
+            }
         },
         processLineChartData(data) {
-            // Process the data similarly to how you processed data for Bar chart
             const colorConfig = {
                 backgroundColor: 'rgba(255, 206, 86, 0.2)',
                 borderColor: 'rgba(255, 206, 86, 1)'
-            };
+            }
 
             return {
                 labels: data.values.map(entry => new Date(entry.x * 1000).toLocaleDateString()),
@@ -147,9 +127,8 @@ export default {
                         data: data.values.map(entry => entry.y)
                     }
                 ]
-            };
+            }
         },
-
     }
 }
 </script>
@@ -159,9 +138,7 @@ export default {
     display: flex;
     flex-direction: column;
     width: 700px;
-    /* Set your desired width here */
     margin: 0 auto;
-    /* To center the container horizontally */
 }
 
 .bar {
